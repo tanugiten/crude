be2de6b3461eed1c8e5a6beb28581f15fff40617
tanugiten <tinakomten@gmail.com>
2019-07-09 15:54:06 +0700

    crude commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.index');
+    }
+    public function __construct() 
+    {
+        $this->middleware(['auth','isAdmin']); //isAdmin middleware lets only users with a //specific permission permission to access these resources
+    }
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', '| Add Role')
+
+@section('content')
+<h1>Welcome to Admin Page</h1>
+
+<a href="users">Roles and Permission</a>
+
+@endsection
